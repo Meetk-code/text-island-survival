1705bb61fbed79162a89dbc363974e77173ecaea
Meetk-code <meet.k.codes@gmail.com>
2026-04-09 06:05:43 +0530

    Refining mess

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
 from player import Player
 import os
 
+# Clear shortcut as a function
 def clear():
+    input()
     os.system("cls" if os.name == "nt" else "clear")
 
+# Main Player
 you = Player(100, 100, 100)
 
+# Constant Variables
 day = 1
 t_days = 2
 choices = 5
 options = ["hunt", "eat", "drink", "sleep"]
 running = True
+alive = True
 
-while day <= t_days:
-    print(f"Day: {day}\n")
-    for I in range(choices):
-        print(f"\nActions remaining : {choices - I}")
-        while True:
-            print("options are:")
-            for I in options:
-                print(f"--> {I}")
-            choice = input("_____________________________\nWhat do you wanna do---> ")
-            if choice in options:
-                you.action(choice)
-                break
-            else:
-                print("Not valid")
-                
-        if you.hunger < 1 or you.water < 1 or you.energy < 1:
-             break
-        you.status()
-        input()
-        clear()
-    day += 1
-    if you.hunger < 1 or you.water < 1 or you.energy < 1:
-                 break
-    you.status()   
-    input()
-    clear()
-   
+# Functions 
+def display_options(left):
+    print(f"\nActions remaining : {choices - left}")
+    print("options are:")
+    for I in options:
+        print(f"--> {I}")
+
+def get_choice():
+    while True:
+        choice = input("_____________________________\nWhat do you wanna do---> ")
+        if choice in options:
+            you.action(choice)
+            break
         
-print("You survived!" if day == t_days + 1 else "You Die")
+        else:
+            print("Not valid, Retry!")
+
+# Main Game Loop
+while day <= t_days and alive:
+       print(f"Day: {day}\n")
+       for I in range(choices):
+        display_options(I)
+        get_choice()
+        you.status()
+        if you.hunger < 1 or you.water < 1 or you.energy < 1:
+            alive = False
+        clear()
+       day+=1
+print("You survived!" if alive else "You Die")
